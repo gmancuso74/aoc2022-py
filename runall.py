17db12131f36e768dc816ab851947890b7f5be3e
charles.mancuso <charles.mancuso@kratosdefense.com>
2023-07-26 10:21:10 -0400

    ignor __pycache__, runall only runs existing solutions

--- runall.py
+++ runall.py
@@ -1,11 +1,14 @@
 import subprocess
 import argparse
+import os.path
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='AOC 2022')
-    parser.add_argument('-s', action='store_true', help='use the small input file')
-    args = parser.parse_args()
-    for i in range(1,26):
-    	file=f'day{i}.py'
-    	proc=subprocess.run(["python", file], capture_output=True, encoding='UTF-8')
-    	print(proc.stdout,end='')
+	parser = argparse.ArgumentParser(description='AOC 2022')
+	parser.add_argument('-s', action='store_true', help='use the small input file')
+	args = parser.parse_args()
+	for i in range(1,26):
+		file=f'day{i}.py'
+		if(os.path.isfile(file)):
+			proc=subprocess.run(["python", file], capture_output=True, encoding='UTF-8')
+			if(proc.stderr): print(f'Day{i}:{proc.stderr}')
+			print(proc.stdout,end='')
